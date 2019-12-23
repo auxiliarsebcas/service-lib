c18bed14b5a9c1086987c3321d235025eaeb20c4
Sebastian Castañeda C <scastane@starbucks.com>
2019-12-23 14:46:15 -0500

    added command, event and service error definitions we have added the basic definitions and signature to declare commands events and errors, beside we have update the .git ignore and the basic configuration to local publish.

--- src/main/java/jobsity/test/command/Command.java
+++ src/main/java/jobsity/test/command/Command.java
@@ -1,6 +1,16 @@
 package jobsity.test.command;
 
-public interface Command {
 
-    
+import cyclops.monads.Witness.*;
+import cyclops.control.Reader;
+import cyclops.monads.transformers.EitherT;
+import jobsity.test.Environment;
+import jobsity.test.Event;
+import jobsity.test.ServiceError;
+
+import java.util.List;
+
+
+public interface Command {
+    public Reader<Environment, EitherT<future,List<ServiceError>,List<Event>>> execute();
 }
